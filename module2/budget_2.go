0b0220e9a7a49294a898a3051e850bf6043606ee
Manjunath_Honnappa <Manjunath_Honnappa@dell.com>
2023-02-05 22:42:24 -0800

    Commited

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -73,12 +73,12 @@ func (b *Budget) RemoveItem(description string) {
 
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
+	if len(report) > 12 {
+		return nil, errReportIsFull
+	}
 	_, hasEntry := report[month]
 	if hasEntry {
 		return nil, errDuplicateEntry
-	}
-	if len(report) > 12 {
-		return nil, errReportIsFull
 	}
 	var newBudget *Budget
 	newBudget = &Budget{Max: max}
